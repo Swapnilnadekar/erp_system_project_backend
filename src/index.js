6e4ab0eb8a25c8c54efa2c5dac2056576dfb3867
Ankit Dhopate <ankit.dhopate@gmail.com>
2021-12-06 11:55:43 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+
+app.use(express.json());
+
+const db = mysql.createConnection({
+  user: "root",
+  host: "localhost",
+  password: "password",
+  database: "erp_project_database",
+  multipleStatements: true,
+});
+
+db.connect((error) => {
+  if (error) {
+    console.log(error);
+  }
+
+  if (!error) {
+    console.log("Connected to the database successfully");
+  }
+});
+
+app.get("/", (req, res) => {
+  res.status(201).json({ msg: "hi" });
+});
